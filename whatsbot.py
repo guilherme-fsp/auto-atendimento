c47752bab8b1a55313c2dbf7fec70839310b207d
Guilherme P <guilhermefsp@outlook.com>
2025-05-04 16:07:18 -0300

    troca do ip host

--- whatsbot.py
+++ whatsbot.py
@@ -105,4 +105,4 @@ def home_page():
     return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='127.0.0.1', port=5000, debug=False)
